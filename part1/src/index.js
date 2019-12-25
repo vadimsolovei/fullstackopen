e38c8eed2d6c70580ba358b28cab7707311605e8
Vadim Solovei <vadims.olovei90@gmail.com>
2019-12-25 10:42:34 +0200

    finished exercise 1.6

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,58 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const Header = ({course}) => {
-  return (
-    <>
-      <h1>{course.name}</h1>
-    </>
-  )
-}
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
-const Content = ({parts}) => {
-  return (
-    <>
-      {parts.map(part => <p>Part: {part.name}, exercise quantity: {part.exercises}</p>)}
-    </>
-  )
-}
+const App = (props) => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-const Total = ({parts}) => {
-  const sum = parts.reduce((result, current) => result + current.exercises, 0)
+  const goodFeedback = () => setGood(good + 1)
 
-  return (
-    <>
-      <p>Total number of exercises: {sum}</p>
-    </>
-  )
-}
+  const neutralFeedback = () => setNeutral(neutral + 1)
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const badFeedback = () => setBad(bad + 1)
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <div>
+        <h1>Feedback</h1>
+        <Button onClick={goodFeedback} text='good' />
+        <Button onClick={neutralFeedback} text='neutral' />
+        <Button onClick={badFeedback} text='bad' />
+      </div>
+      <div>
+        <h1>Statistics</h1>
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+      </div>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
